3f47ca8bd07f85d6f9fb0b09e94fcaba9d9394be
phitchcock <phitchcock@me.com>
2014-05-13 12:03:37 -0700

    added image to Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,7 +56,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit!
   end
 
   def set_post
